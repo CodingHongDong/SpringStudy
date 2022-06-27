295af7d071e35c5f8604422a409c392ce0c302bd
HongDongHwan <hongdh94@gmail.com>
2022-06-27 19:04:52 +0900

    Revert "Revert "ADD 0627 Spring DI 방식 study""
    This reverts commit c56cdafd61abc518e10eec7c9addfa7fa2c1a438.

--- src/spring/di/Program.java
+++ src/spring/di/Program.java
@@ -1,8 +1,14 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
@@ -15,11 +21,22 @@ public class Program {
 //		
 //		console.setExam(exam);
 		
-		ApplicationContext context = 
-				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext(NewlecDIConfig.class); // <- Java Configuration방식
+				// new ClassPathXmlApplicationContext("spring/di/setting.xml"); <- XML Configuration방식
+		
+		// Exam exam = context.getBean(Exam.class);
+		// System.out.println(exam.toString());
 		
 		ExamConsole console = (ExamConsole) context.getBean("console");
 		// ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
+		// List<Exam> exams = (List<Exam>) context.getBean("exams"); // new ArrayList<>();
+		// exams.add(new NewlecExam(1, 1, 1, 1));
+		
+		// for(Exam e : exams)
+		//	System.out.println(e);
+		
 	}
 }
