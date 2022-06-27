658bb081fa9035a54ca25e1db541ef103c4fd35e
HongDongHwan <hongdh94@gmail.com>
2022-06-27 18:49:58 +0900

    ADD 0627 Spring DI 방식 study
    body : 10 - 생성자 DI
    property or constructor-arg
    11 - 콜렉션 생성과 목록 DI
    <util:list class="?">
    12 - 어느테이션을 이용할 때의 장점과 @Autowired를 이용한 DI해보기
    * 주석처리 확실하게 하기 ㅠㅠ
    13 - @Autowired의 동작방식 이해와 @Qualifier 사용하기
    1순위는 자료형 2순위는 변수명 - > 동작방식 순서
    @Qualifier -> 특정 변수명을 갖는 객체를 연결하게 해준돠잉! 그래서 같이쓰자
    14 - @Autowired의 위치와  required 옵션
    @Autowired 인잭션 방법3가지
    1) 필드 위
    2) 생성자
    3) 세터인잭션
    15 - 어노테이션을 이용한 객체생성
    16 - @Component의 종류과 시멘틱 @Component
    17 - XML Configuration을 Java Configuration

--- src/spring/di/entity/NewlecExam.java
+++ src/spring/di/entity/NewlecExam.java
@@ -1,11 +1,28 @@
 package spring.di.entity;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 public class NewlecExam implements Exam {
 	
+	@Value("20")
 	private int kor;
+	@Value("30")
 	private int eng;
 	private int math;
 	private int com;
+	
+	public NewlecExam() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public NewlecExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
 	
 	public int getKor() {
 		return kor;
@@ -47,6 +64,11 @@ public class NewlecExam implements Exam {
 	@Override
 	public float avg() {
 		return total() / 4.0f;
+	}
+
+	@Override
+	public String toString() {
+		return "NewlecExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
 	}
 	
 }
